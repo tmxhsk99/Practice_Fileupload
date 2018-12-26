930eac45807339071dbd97ebc3a446fb171760d2
kimjuhyeon <tmxhsk99@naver.com>
2018-12-26 14:06:34 +0900

    thumbnail_Ajax

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -1,14 +1,28 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.AttachFileDTO;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -36,5 +50,100 @@ public class UploadController {
 				log.error(e.getMessage());
 			}
 		}
+	}
+	
+	@GetMapping("/uploadAjax")
+	public void uploadAjax() {
+		log.info("uploadAjax");
+		
+	}
+	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+		
+		return str.replace("-",File.separator);
+		
+	}
+	
+	@PostMapping(value= "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
+		
+		List<AttachFileDTO> list = new ArrayList<>();
+		String uploadFolder = "C:\\upload";
+		
+		// make folder ----------------------
+		File uploadPath = new File(uploadFolder,getFolder());
+		log.info("upload path : "+uploadPath);
+		
+		
+		String uploadFolderPath = getFolder();
+		if(uploadPath.exists()== false) {
+			uploadPath.mkdirs();
+		}
+		//make yyyy/MM/dd folder
+			
+		for(MultipartFile multipartFile : uploadFile) {
+			log.info("----------------------------------------------------");
+			log.info("Upload File Name : "+multipartFile.getOriginalFilename());
+			log.info("Upload File Size: "+ multipartFile.getSize());
+			
+			AttachFileDTO attachDTO = new AttachFileDTO();//브라우저에 보낼 정보
+			
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			//IE has file path
+			/*uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\")+1);*/
+			log.info("only file name : " + uploadFileName);
+			attachDTO.setFileName(uploadFileName);
+			
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			try {
+			File saveFile = new File(uploadPath, uploadFileName);
+			multipartFile.transferTo(saveFile);
+			
+			attachDTO.setUuid(uuid.toString());
+			attachDTO.setUploadPath(uploadFolderPath);
+			
+			//check image type file
+			if (checkImageType(saveFile)) {
+				attachDTO.setImage(true);
+				
+				FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath,"s_"+uploadFileName));
+				
+				Thumbnailator.createThumbnail(multipartFile.getInputStream(),thumbnail,100,100);
+				
+				thumbnail.close();
+			}
+				//add to List
+			list.add(attachDTO);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
+		}//end for
+		
+		return new ResponseEntity<>(list,HttpStatus.OK);
+	}
+	
+	private boolean checkImageType(File file) {
+		
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			
+			return contentType.startsWith("image");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+		
 	}
 }
